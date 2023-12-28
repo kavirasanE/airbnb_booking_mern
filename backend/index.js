a26bb571ed508de6f3f4777716108dbc961e6211
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2023-12-28 07:55:32 +0530

    login adn register

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,56 @@
 import express  from 'express'
+import cors  from 'cors'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import  bcrypt from 'bcryptjs'
+import User from './models/User.js';
 
+dotenv.config()
+ const bcryptSalt = await bcrypt.genSalt(10);
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:5173'
+}));
 app.get('/test' ,(req,res) => {
     res.json('ok')
 });
 
-app.listen(9000 ,() => {
+app.post('/register',async (req,res) => {
+    const {name,email,password} =req.body;
+    try{
+        const userDoc = await User.create({
+            name,
+            password:bcrypt.hashSync(password,bcryptSalt),
+            email:bcrypt.hashSync(email,bcryptSalt),
+         });
+         res.json(userDoc);
+    }
+    catch(err){
+        res.status(400).json(err);
+    }
+});
+
+app.post ('/login' , async (req,res) => {
+    const {email,password} =req.body;
+    const userDoc =await User.findOne({email})
+    if(userDoc) {
+        const passOk = bcrypt.compareSync(password,userDoc.password)
+        if(passOk) {
+            res.json('pass ok')
+        }else{
+            res.json('pass not ok')
+        }
+        res.json("found")
+    } else {
+        res.json('not found ')
+    }
+   
+})
+
+app.listen(4000 ,() => {
     console.log('server')
 });
+mongoose.connect(process.env.MONGO_URL);
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,22 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLoginSubmit (ev) {
+    ev.preventDefault();
+    try{
+        await axios.post('/login', {email,password});
+        alert('login')
+    }
+    catch(err) {
+      alert(err);
+    }
+  }
   return (
-    <div className='mt-4 grow flex items-center justify-around '>
-       <div className='mb-64'>
-       <h1 className='text-4xl  text-center mb-5'>Login</h1>
-     <form className='max-w-lg mx-auto '>
-        <input type='email' placeholder='your@email.com' />
-        <input type='password' placeholder='Enter your Password' />
-        <button className='primary'>Login</button>
-        <div className='text-center py-2  text-gray-500'>Don't have  an  account yet?  <Link className="underline text-black font-semibold " to={"/register"}>Register Now</Link>
-         </div>
-
-     </form>
-       </div>
+    <div className="mt-4 grow flex items-center justify-around ">
+      <div className="mb-64">
+        <h1 className="text-4xl  text-center mb-5">Login</h1>
+        <form className="max-w-lg mx-auto " onSubmit={{handleLoginSubmit}}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(i) => setEmail(i.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter your Password"
+            value={password}
+            onChange={(i) => setPassword(i.target.value)}
+          />
+          <button className="primary">Login</button>
+          <div className="text-center py-2  text-gray-500">
+            Don't have an account yet?{" "}
+            <Link
+              className="underline text-black font-semibold "
+              to={"/register"}
+            >
+              Register Now
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
